c263d9c93fa7af92ba46595ed07476386c9c89cc
Richard <richardconey46@gmail.com>
2023-02-15 22:50:48 -0500

    Strings Challenges

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,15 +1,29 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  if(str.indexOfLetter === -1) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function cap(str, letter) {
-
+   const upper = str.toUpperCase(2);
+   const index = upper.slice(upper.indexOf(letter + 1));
+   if (indexOfLetter === +1) {
+    return 'Sorry, letter not found'
+   }
 }
 
 function firstCharacter(str1, str2) {
-
+  const Start1 = str1[0];
+  const Start2 = str2[0];
+  if (str1 === str2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
