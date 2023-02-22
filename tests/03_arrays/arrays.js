d25652a30ca13506cc35aa5cfb1778bb218b770c
Richard <richardconey46@gmail.com>
2023-02-22 06:42:14 -0500

    Arrays Challenges

--- tests/03_arrays/arrays.js
+++ tests/03_arrays/arrays.js
@@ -1,22 +1,35 @@
 /* eslint-disable no-unused-vars */
 function multiply(arr) {
-
+const flatArr = arr.flat(multiply);
+console.log(flatArr);
 }
 
 function includesCopy(arr, searchValue) {
-
+  const doesInclude = includesCopy(arr);
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === searchValue) {
+      return true
+    }
+    return false
+  }
 }
 
 function inventory(arr) {
-
+ const newArr = (items);
+ const arr1 = (returnedValue);
+ const arr2 = newArr.concat(arr1);
+ console.log(arr2);
 }
 
 function camelCase(str) {
-
+ const str = returnedValue.split('');
+ const str1 = returnedValue.join('');
+ console.log(str);
 }
 
-function joiner() {
-
+function joiner() {;
+const str = arr.join('returnedValue');
+console.log(str);
 }
 
 module.exports = {
