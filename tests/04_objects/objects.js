a6911b594b29f3cfc92cebd6d3510219baf5e9b3
Richard <richardconey46@gmail.com>
2023-02-24 18:31:12 -0500

    object challenges

--- tests/04_objects/objects.js
+++ tests/04_objects/objects.js
@@ -2,19 +2,49 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function deleteProp(obj, prop) {
-
+ delete objectItems.firstName;
+ console.log(objectItems.firstName);
+ delete objectItems.age;
+ console.log(objectItems.age);
+ objectItems.firstName(Tom);
+ console.log(objectItems.firstName);
+ objectItems.age(25);
+ console.log(objectItems.age);
+ console.log(objectItems);
 }
 
 function numObjectProps(obj) {
-
+  delete objectItems.age;
+  console.log(objectItems.age);
+  delete objectItems.occupation;
+  console.log(objectItems.occupation);
+  let returnedValue = 2;
+ for(let i = 0; i <= objectItems.length; i++) {
+  return returnedValue;
+ }
+ console.log(objectItems);
 }
 
 function shoppingCart(cart) {
+  const values = cartItems.amount;
+  const sum = values.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0) 
+  console.log(sum);
 
+  const totalObj = sum(obj1, obj2);
+  if(obj1 !== obj2) {
+    return false
+  } else  {
+    return true
 }
 
 function compareObjects(obj1, obj2) {
-
+  if(obj1 === obj2) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = {
